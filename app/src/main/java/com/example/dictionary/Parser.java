af14ae523543702438aa797153d6b73794535f5a
snegi0 <www.ipostoyuk@gmail.com>
2021-12-08 20:02:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dictionary/Parser.java
@@ -0,0 +1,12 @@
+package com.example.dictionary;
+
+public class Parser {
+    private String Text;
+    private String Word;
+    private String URL;
+
+    public String getText() {
+        return Text;
+    }
+
+}
